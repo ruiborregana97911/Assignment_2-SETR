643de5ae3daa4f899e23345d24a3470719524417
Rui Borregana <ruiborregana@ua.pt>
2025-03-29 12:07:32 +0000

    adaptacoes para usar o unity

---
+++ sampleCode/test/test_cmdproc.c
@@ -0,0 +1,27 @@
+#include <unity.h>
+#include "cmdproc.h"
+#include "sensores.h"
+
+
+void setUp(void)
+{
+		
+	return;
+}
+void tearDown(void)
+{
+	return;
+}
+
+
+
+
+
+
+int main(void)
+{
+	UNITY_BEGIN();
+	
+	
+	return UNITY_END();
+}
